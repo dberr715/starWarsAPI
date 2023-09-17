c2b478b9aee6d0b415cef7a0f21055404da1924a
David Berry <dberry3755@gmail.com>
2023-09-17 16:16:09 -0400

    dropdown menu working as intended

--- scripts/main.js
+++ scripts/main.js
@@ -1,62 +1,69 @@
 'use strict';
-const starshipListElement = document.querySelector('#swinfo');
-const starshipListElement2 = document.querySelector('#starshipInfo');
 
-document.addEventListener("DOMContentLoaded", function (){
-    console.log ("We have lift off!");
-
-const swInfo = document.getElementById('swInfo');
-const swInfo2 = document.getElementById('swInfo2');
-console.log(swInfo)
-const random = parseInt(Math.random()*10) 
-const notZero = random ? random: 3
-console.log(notZero)
-console.log((random))
-    fetch(`https://swapi.dev/api/vehicles/`)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data){
-        function wantVehicle(vehicle, element) {
-            element.innerText = vehicle
-        };
-        wantVehicle(data.results[notZero].name, swInfo);
-        // console.log(data);
-        return data;
-    })
-//     fetch('https://swapi.dev/api/starships/')
-//     .then(function(response){
-//         return response.json();
-//     })
-//     .then(function(data){
-//        function wantStarship(starship, element2) {
-// element2.innerText = starship;
-// };
-//         wantStarship(data.results[notZero].name, swInfo2);
-//         return data;
-
-//     })
-//     const starshipUrl = 'https://swapi.dev/api/starships/';
-
-//     get(starshipUrl).then(function (response) {
-//         console.log("Starship LIST:", response.results);
-//         makeListOfShips(response.results.map(x => {
-//             console.log(x.name);
-//             return x.name;
-//    }));
-
-
-
- function makeListOfShips(starshipArray){
-    console.log(starshipArray)
-    const selectElement = document.createElement('select');
-    starshipArray.map(function(starship){
-        const option = document.createElement('option');
-        option.value = starship;
-        option.text = starship;
-        selectElement.appendChild(option);
-        starshipListElement2.append(selectElement);
-    })
+function showPassengers(x) {
+  document.getElementById("passengerCount").innerText = x;
 }
-    });
 
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("We have lift off!");
+
+  const starshipListElement = document.querySelector('#starshipInfo');
+  const updateButton = document.querySelector('#updateButton'); // Select the button by its ID
+  const swInfo = document.getElementById('swInfo');
+
+  // Fetch the list of starships from the API
+  fetch("https://swapi.dev/api/starships/")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const starships = data.results;
+
+      // Create the select element for the dropdown
+      const selectElement = document.createElement('select');
+
+      starships.forEach(function (starship) {
+        const option = document.createElement('option');
+        option.value = starship.name;
+        option.text = starship.name;
+        selectElement.appendChild(option);
+      });
+
+      // Append the select element to the DOM
+      starshipListElement.appendChild(selectElement);
+
+      // Event listener for when a starship is selected
+      selectElement.addEventListener("change", function () {
+        const selectedStarshipName = selectElement.value;
+        const selectedStarship = starships.find(function (starship) {
+          return starship.name === selectedStarshipName;
+        });
+
+        if (selectedStarship) {
+          showPassengers(selectedStarship.passengers);
+        }
+      });
+
+      // Select a random starship when the content is loaded
+      const randomStarshipIndex = Math.floor(Math.random() * starships.length);
+      const randomStarship = starships[randomStarshipIndex];
+      
+      if (randomStarship) {
+        showPassengers(randomStarship.passengers);
+      }
+
+      // Event listener for when the button is clicked
+      updateButton.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent the form submission
+        const randomStarshipIndex = Math.floor(Math.random() * starships.length);
+        const randomStarship = starships[randomStarshipIndex];
+        
+        if (randomStarship) {
+          showPassengers(randomStarship.passengers);
+        }
+      });
+    })
+    .catch(function (error) {
+      console.error("Error fetching starships:", error);
+    });
+});
